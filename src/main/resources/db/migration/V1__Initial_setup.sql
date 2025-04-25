28ac28e9d5f5103e6f96aeb7f3f42eca2ce04124
JoseLucasdevv <joselucas.dev@gmail.com>
2025-04-25 15:53:39 -0300

    feat:add Universal GUID UUID

--- src/main/resources/db/migration/V1__Initial_setup.sql
+++ src/main/resources/db/migration/V1__Initial_setup.sql
@@ -1,5 +1,6 @@
 CREATE TABLE url (
     id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    uid CHAR(36) NOT NULL DEFAULT (UUID()),
     short_url varchar(50) NOT NULL,
     original_url varchar(255) NOT NULL,
     expires_at timestamp NOT NULL,
