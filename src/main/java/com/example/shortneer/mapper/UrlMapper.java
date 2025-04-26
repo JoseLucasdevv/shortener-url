cbffb737423c07f62deeb3b41ebf0b380f9541d3
JoseLucasdevv <joselucas.dev@gmail.com>
2025-04-26 13:51:52 -0300

    feat: create UrlMapper.java

---
+++ src/main/java/com/example/shortneer/mapper/UrlMapper.java
@@ -0,0 +1,16 @@
+package com.example.shortneer.mapper;
+
+import com.example.shortneer.domain.URL;
+import com.example.shortneer.http.response.UrlResponse;
+
+public class UrlMapper {
+
+    
+
+    public static UrlResponse urlToUrlResponse(URL url){
+
+
+        return new UrlResponse(url.getShortUrl(), url.getOriginalUrl(), url.getUid().toString(), url.getExpiresAt(), url.getCreatedAt());
+    }
+    
+}
