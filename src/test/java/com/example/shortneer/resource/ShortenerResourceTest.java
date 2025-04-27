6f465f33618afc783e2478d1e7e19f33a12d50d7
JoseLucasdevv <joselucas.dev@gmail.com>
2025-04-27 11:13:23 -0300

    feat:add useCase Success and failure to the Post verb from shortenerResource

--- src/test/java/com/example/shortneer/resource/ShortenerResourceTest.java
+++ src/test/java/com/example/shortneer/resource/ShortenerResourceTest.java
@@ -1,19 +1,18 @@
 package com.example.shortneer.resource;
 
-import java.time.LocalDateTime;
-
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
-
-import com.example.shortneer.domain.URL;
+import com.example.shortneer.http.response.UrlResponse;
 import com.example.shortneer.repository.URLRepository;
+import com.example.shortneer.service.URLService;
 
-import util.GenerateRandomToken;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -21,8 +20,38 @@ class ShortenerResourceTest {
 
     @Autowired
     private MockMvc mockMvc;
-    @Autowired
+    @Mock
     private URLRepository urlRepository;
+
+    @Autowired
+    @InjectMocks
+    URLService urlService;
+    @Test
+  void testShortenerPostEndpointFailCase() throws Exception {
+    String json = "{\"url\":\"htt\"}";
+    
+    mockMvc.perform(MockMvcRequestBuilders.post("http://localhost:8080/api/v1/shortener")
+    .contentType("application/json;charset=UTF-8")
+    .content(json))
+    .andExpect(MockMvcResultMatchers.status().isBadRequest())
+    .andExpect(MockMvcResultMatchers.content().contentType("application/json"))
+    .andExpect(MockMvcResultMatchers.jsonPath("error").value("Validation URL exception")).andReturn();
+       
+  }
+
+  @Test
+  void testShortenerPostEndpointSuccessCase() throws Exception {
+    String json = "{\"url\":\"http:www.google.com\"}";
+    
+    mockMvc.perform(MockMvcRequestBuilders.post("http://localhost:8080/api/v1/shortener")
+    .contentType("application/json;charset=UTF-8")
+    .content(json))
+    .andExpect(MockMvcResultMatchers.status().isCreated())
+    .andExpect(MockMvcResultMatchers.content().contentType("application/json")).andReturn();
+       
+  }
+
+
     @Test
   void testShortenerGetEndpointFailCase() throws Exception {
     mockMvc.perform(MockMvcRequestBuilders.get("http://localhost:8080/api/v1/shortener"))
@@ -38,13 +67,11 @@ class ShortenerResourceTest {
 
   @Test
   void testShortenerGetEndpointSuccessCase() throws Exception {
-    URL url = new URL();
-    url.setOriginalUrl("https://www.google.com");
     
-    url.setShortUrl("http://localhost:8080/api/v1/shortener?token=" + GenerateRandomToken.build());
-    url.setExpiresAt(LocalDateTime.now().plusMinutes(30));
-    this.urlRepository.save(url);
-    mockMvc.perform(MockMvcRequestBuilders.get(url.getShortUrl()))
+    UrlResponse response = this.urlService.save("https://google.com");
+    
+    
+    mockMvc.perform(MockMvcRequestBuilders.get(response.shortUrl()))
        .andExpect(MockMvcResultMatchers.status()
        .isFound());
   }
