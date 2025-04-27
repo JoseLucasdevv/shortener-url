111c84f3b5a074f61314873727fcddf6fe555066
JoseLucasdevv <joselucas.dev@gmail.com>
2025-04-27 10:32:32 -0300

    feat:create private default constructor.

--- src/main/java/util/GenerateRandomToken.java
+++ src/main/java/util/GenerateRandomToken.java
@@ -2,7 +2,9 @@ package util;
 
 public class GenerateRandomToken {
 
-public static String Build(){
+    private GenerateRandomToken(){}
+
+public static String build(){
     final String numbersAndLetters = "0123456789ABCDEFGHIJKLMNOPQRSTUVXZabcdefghijklmnopqrstuvxzwy";
     StringBuilder stringBuilder = new StringBuilder();
     for(int i = 0 ; i < 6; i++) {
