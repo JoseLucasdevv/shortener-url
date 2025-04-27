8bf314284bafb497c52f7e948987c154865fc2f7
JoseLucasdevv <joselucas.dev@gmail.com>
2025-04-26 21:12:54 -0300

    feat: create logic from ShortenerUriResource

--- src/main/java/com/example/shortneer/http/api/ShortenerUriResource.java
+++ src/main/java/com/example/shortneer/http/api/ShortenerUriResource.java
@@ -1,10 +1,17 @@
 package com.example.shortneer.http.api;
 
+import com.example.shortneer.exceptions.ParameterException;
 import com.example.shortneer.http.request.UrlRequest;
 import com.example.shortneer.http.response.UrlResponse;
 import com.example.shortneer.service.URLService;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
+
+import java.net.URI;
+import java.security.InvalidParameterException;
+import java.util.Optional;
+
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
@@ -23,19 +30,21 @@ public ShortenerUriResource(URLService urlService){
 
     @PostMapping("shortener")
     public ResponseEntity<UrlResponse> create(@RequestBody @Valid UrlRequest urlRequest){
-    
         UrlResponse response = this.urlService.save(urlRequest.url());
-
-        return ResponseEntity.ok().body(response);
+        URI location = URI.create("http://localhost");
+        return ResponseEntity.created(location).body(response);
 
     }
 
 
-
     @GetMapping("shortener")
-    public ModelAndView redirect(@RequestParam("token") String shortUrl, HttpServletResponse httpServletResponse){
+    public ModelAndView redirect(@RequestParam(name = "token" , required = false) Optional<String> shortUrl, HttpServletResponse httpServletResponse){
 
-        return new ModelAndView("redirect:" + httpServletResponse.encodeRedirectURL("https://google.com"));
+            if(!shortUrl.isPresent()) throw new ParameterException("Invalid Parameter");
+
+            String response = this.urlService.verifyShortenerUrl(shortUrl.get());
+
+        return new ModelAndView("redirect:" + httpServletResponse.encodeRedirectURL(response));
     }
 
 }
