a5c459156a33db3655963272048d65f82d2dc204
JoseLucasdevv <joselucas.dev@gmail.com>
2025-04-26 21:12:10 -0300

    feat: create ParameterException.class

---
+++ src/main/java/com/example/shortneer/exceptions/ParameterException.java
@@ -0,0 +1,8 @@
+package com.example.shortneer.exceptions;
+
+public class ParameterException extends RuntimeException { 
+    public ParameterException(String message) {
+        super(message);
+    }
+}
+
