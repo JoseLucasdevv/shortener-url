5b5dec4551888db991de3096263552d4ca9d9a95
JoseLucasdevv <joselucas.dev@gmail.com>
2025-04-27 12:01:37 -0300

    lint:remove unnecessary imports

--- src/test/java/com/example/shortneer/repository/URLRepositoryTest.java
+++ src/test/java/com/example/shortneer/repository/URLRepositoryTest.java
@@ -8,13 +8,9 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.cglib.core.Local;
-
 import com.example.shortneer.domain.URL;
-
 import jakarta.persistence.EntityManager;
-import net.bytebuddy.dynamic.DynamicType.Builder.FieldDefinition.Optional;
+
 
 
 @DataJpaTest
