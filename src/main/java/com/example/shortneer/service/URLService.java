01f05659c0107f749260bbe99a7ad3f346264817
JoseLucasdevv <joselucas.dev@gmail.com>
2025-04-26 21:13:32 -0300

    feat: create method verifySHortenerUrl

--- src/main/java/com/example/shortneer/service/URLService.java
+++ src/main/java/com/example/shortneer/service/URLService.java
@@ -3,8 +3,10 @@ package com.example.shortneer.service;
 import java.time.LocalDateTime;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
 
 import com.example.shortneer.domain.URL;
+import com.example.shortneer.exceptions.Exception;
 import com.example.shortneer.http.response.UrlResponse;
 import com.example.shortneer.mapper.UrlMapper;
 import com.example.shortneer.repository.URLRepository;
@@ -32,8 +34,18 @@ public class URLService {
         URL urlFromDB =  this.urlRepository.save(urlDomain);
 
         return UrlMapper.urlToUrlResponse(urlFromDB);
+        
+    }
+
+    public String verifyShortenerUrl(String shortUrl){
+        String newUrl = baseurl + "?token=" + shortUrl;
+        URL url = this.urlRepository.findByShortUrl(newUrl).orElseThrow(() -> new Exception("we couldn't find this shortUrl",HttpStatus.NOT_FOUND));
+
+        if(url.getExpiresAt().isBefore(LocalDateTime.now())) throw new Exception("shortUrl was expired.", HttpStatus.BAD_REQUEST);
 
         
+
+        return url.getOriginalUrl();
     }
 
 
