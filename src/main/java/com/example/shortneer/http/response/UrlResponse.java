33afe0ea57cead6ee17600907e5a1f6f89271d1e
JoseLucasdevv <joselucas.dev@gmail.com>
2025-05-17 10:22:35 -0300

    feat: add serialization json format dates

--- src/main/java/com/example/shortneer/http/response/UrlResponse.java
+++ src/main/java/com/example/shortneer/http/response/UrlResponse.java
@@ -1,5 +1,12 @@
 package com.example.shortneer.http.response;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import org.springframework.hateoas.RepresentationModel;
+
 import java.time.LocalDateTime;
 
-public record UrlResponse(String shortUrl, String OriginalUrl,String uid,LocalDateTime expiresAt,LocalDateTime createdAt) {}
+public record UrlResponse(String shortUrl, String OriginalUrl, String uid, @JsonFormat(pattern = "yyyy-MM-dd HH:mm") LocalDateTime expiresAt, @JsonFormat(pattern = "yyyy-MM-dd HH:mm") LocalDateTime createdAt)  {
+
+
+
+}
